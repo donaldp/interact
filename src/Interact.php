53343a77d969709d51d298a5fb0ad20013e896ed
donald_p <donaldpakkies@gmail.com>
2019-04-22 16:07:37 +0200

    feat: created a new Modulus plugin

---
+++ src/Interact.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Don47\Interact;
+
+use Modulus\Utility\Plugin;
+
+class Interact extends Plugin
+{
+  /*
+  |--------------------------------------------------------------------------
+  | Plugin information
+  |--------------------------------------------------------------------------
+  */
+
+  /**
+   * Interact::NAME
+   */
+  const PACKAGE = 'Modulus Interact';
+
+  /**
+   * Interact::VERSION
+   */
+  const VERSION = '0.1';
+
+  /**
+   * Interact::FRAMEWORK
+   */
+  const FRAMEWORK = '1.9.*|2.*';
+
+  /**
+   * Interact::AUTHORS
+   */
+  const AUTHORS = [
+    'Donald Pakkies' => 'donaldpakkies@gmail.com'
+  ];
+}
+
