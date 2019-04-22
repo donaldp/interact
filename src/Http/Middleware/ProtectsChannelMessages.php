3b9af7db5a7c71a1197faea8ff5ff064e9ac8f99
donald_p <donaldpakkies@gmail.com>
2019-04-22 16:19:54 +0200

    feat: created a new middleware

---
+++ src/Http/Middleware/ProtectsChannelMessages.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Don47\Interact\Http\Middleware;
+
+use Don47\Interact\Interact;
+
+class ProtectsChannelMessages
+{
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Modulus\Http\Request $request
+   * @param  bool $continue
+   * @return bool $continue
+   */
+  public function handle($request, $continue) : bool
+  {
+    //
+  }
+}
