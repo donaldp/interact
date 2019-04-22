6d4d8227f27a0ea95c1aa6095b8793daff2e2158
donald_p <donaldpakkies@gmail.com>
2019-04-22 16:16:13 +0200

    feat: Validate request and return a new message object

--- src/Http/Controllers/Action.php
+++ src/Http/Controllers/Action.php
@@ -8,5 +8,25 @@ use Don47\Interact\Message;
 
 class Action
 {
-  //
+  /**
+   * Message Model
+   *
+   * @var Message|null
+   */
+  protected $message;
+
+  /**
+   * Validate request and return a new message object
+   *
+   * @param Request $request
+   * @return Message|null
+   */
+  private function message(Request $request)
+  {
+    $request->rules = [ 'channel' => 'required' ];
+
+    $request->validate();
+
+    return ($this->message = Message::where('name', strtolower($request->channel))->first());
+  }
 }
